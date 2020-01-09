be07e527542628ed3e9532ca38603664c4939a95
simonbogaerts <simon.bogaerts@outlook.com>
2020-01-09 17:05:23 +0100

    async start

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,37 +5,14 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursePage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
-  handleChange = event => {
-    const _course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: _course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,23 @@
 import * as types from "../constants/courseActionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, data: course };
+}
+
+export function loadCoursesSucces(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, data: courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(cources => {
+        dispatch(loadCoursesSucces(cources));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
